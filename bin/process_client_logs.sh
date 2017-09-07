818e30ada2a301422fae0f7c9cd88adc38b39216
casey36901 <casey369@morris.umn.edu>
2017-09-07 16:57:33 -0500

    process_client_logs.sh tests pass and the script functions properly

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd $1
-grep -rh .| awk '/Failed password for invalid user/{print $1, $2,substr($3,1,2), $11, $13}' > temp.txt
 
-grep -rh .| awk '/Failed password for [^invalid]/{print $1, $2,substr($3,1,2), $9, $11}' > temp2.txt
+grep -rh .| awk '/Failed password for/{
+if ($9 == "invalid")
+    print $1, $2,substr($3,1,2), $11, $13;
+else
+    print $1, $2,substr($3,1,2), $9, $11;
+}' | cat > failed_login_data.txt
 
-cat temp.txt temp2.txt > failed_login_data.txt
-
-rm temp.txt temp2.txt
 
 cp failed_login_data.txt ~
