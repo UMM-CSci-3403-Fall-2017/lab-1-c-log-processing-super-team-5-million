60725ffa8b26b0970e09d8d086aa7524046fad39
casey36901 <casey369@morris.umn.edu>
2017-09-07 20:30:15 -0500

    Adds comments to process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd $1
-
+#Searches through the log files recursivly for failed logins
+#If statement filters between valid users and invalid users
+#And cats it to failed_login_data.txt
 grep -rh .| awk '/Failed password for/{
 if ($9 == "invalid")
     print $1, $2,substr($3,1,2), $11, $13;
