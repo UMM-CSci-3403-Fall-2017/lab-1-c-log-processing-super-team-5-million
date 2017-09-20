9314a1d79cb82e57da021f446d30a104a0f1b9fe
Philip Blaskowski <blask017@morris.umn.edu>
2017-09-19 19:48:20 -0500

    Adds comments to process_logs.sh

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,6 +2,8 @@
 mkdir temp_dir
 home=$(pwd)
 counter=1
+#loops through all of the tar files given and creates a seperate folder for
+#the extracted contents of each and runs process_client_logs.sh on each of them
 for var in "$@"
 do
 	mkdir temp_dir/$counter
@@ -9,7 +11,7 @@ do
         ./bin/process_client_logs.sh temp_dir/$counter
 	counter=$((counter + 1))	
 done
-
+#Runs all of the required scripts to stitch together the final report
 ./bin/create_username_dist.sh $home/temp_dir
 ./bin/create_hours_dist.sh $home/temp_dir
 ./bin/create_country_dist.sh $home/temp_dir
