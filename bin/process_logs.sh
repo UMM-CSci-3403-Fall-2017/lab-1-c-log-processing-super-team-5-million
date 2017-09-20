c4868a0e74913f78499f0d433d7c244268f76382
Philip Blaskowski <blask017@morris.umn.edu>
2017-09-19 19:32:37 -0500

    Finishes process_logs.sh with tests passing

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -6,8 +6,13 @@ for var in "$@"
 do
 	mkdir temp_dir/$counter
 	tar -xf $var -C temp_dir/$counter
+        ./bin/process_client_logs.sh temp_dir/$counter
 	counter=$((counter + 1))	
 done
 
-./bin/process_client_logs.sh temp_dir
-mv temp_dir/failed_login_data.txt $home
+./bin/create_username_dist.sh $home/temp_dir
+./bin/create_hours_dist.sh $home/temp_dir
+./bin/create_country_dist.sh $home/temp_dir
+./bin/assemble_report.sh $home/temp_dir
+mv temp_dir/failed_login_summary.html $home
+rm -r temp_dir
