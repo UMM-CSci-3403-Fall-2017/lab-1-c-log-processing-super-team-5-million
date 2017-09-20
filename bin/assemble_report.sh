7d414020de3114ea216508fd69241cdc5364f714
Philip Blaskowski <blask017@morris.umn.edu>
2017-09-19 19:52:43 -0500

    Adds comments

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 home=$(pwd)
 cd $1
+#Stitches all of the seperate html files into one
 cat  country_dist.html hours_dist.html username_dist.html  >> report_temp.txt
 mv report_temp.txt $home/html_components
 cd $home
 cp bin/wrap_contents.sh $home/html_components
 cd $home/html_components
+#Runs wrap_contents.sh on the required files to create failed_login_summary.html
 ./wrap_contents.sh report_temp.txt summary_plots failed_login_summary.html
+#removes temp files
 rm report_temp.txt
 rm wrap_contents.sh
 mv failed_login_summary.html $1
--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#Takes the input text and wraps it in the properly named html files#Takes the input text and wraps it in the properly named html files#Takes the input text and wraps it in the properly named html files
 cat $2_header.html $1 $2_footer.html > $3
